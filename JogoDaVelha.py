a66baa9cb28300f8625bacd7eddc5218e902a860
Leonardo Ferreira <leo.coelhoferreira@gmail.com>
2023-07-16 17:37:26 -0300

    adicionar arquivos

---
+++ JogoDaVelha.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import sys
+from random import randint
+WIN_SIZE = 900
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode([WIN_SIZE] * 2)
+        self.clock = pg.time.Clock()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type ==pg.QUIT:
+                pg.quit()
+                sys.exit()
+    def run(self):
+        while True:
+            self.check_events()
+            pg.display.update()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
